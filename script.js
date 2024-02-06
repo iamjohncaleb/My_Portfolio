681563683dc2214a044e269d91f048447f98dc90
Caleb John <johncaleb022@gmail.com>
2024-02-05 23:48:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
